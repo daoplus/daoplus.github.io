b002940f160843bba0306452d3c723c6e94fc612
Yang SHEN <geextudio@outlook.com>
2022-11-20 18:11:27 +0800

    demo 003

--- assets/eagerComputed.28618096.js
+++
@@ -1,1 +0,0 @@
-import{ap as t,W as r}from"./index.fcda819e.js";function o(a){var e=t();return r(function(){e.value=a()},{flush:"sync"}),e}export{o as e};
---
+++ assets/eagerComputed.360256ba.js
@@ -0,0 +1,1 @@
+import{ap as t,W as r}from"./index.61d2a47c.js";function o(a){var e=t();return r(function(){e.value=a()},{flush:"sync"}),e}export{o as e};
